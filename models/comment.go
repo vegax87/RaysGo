7958a0c1baa10503e27c7259bd9831f03ff25b0f
Raysmond <jiankunlei@126.com>
2014-01-14 04:03:53 +0800

    add models (TODO)

---
+++ models/comment.go
@@ -0,0 +1,20 @@
+package models
+
+// TODO
+
+func AddComment(title string, content string, uid int64, postId int64, pid int64) (int64,error){
+	return 0, nil
+}
+
+func ActiveComment(id int64) error{
+	return nil
+}
+
+func BlockComment(id int64) error{
+	return nil
+}
+
+func DelComment(id int64) error{
+	return nil
+}
+
---
+++ models/comment_form.go
@@ -0,0 +1,11 @@
+package models
+
+// TODO
+
+type CommentForm struct{
+	Title string
+	Content string 
+	UserName string
+	UserEmail string
+	UserHomePage string
+}
